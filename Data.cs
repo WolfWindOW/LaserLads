44b5fb7e7a7d71a576708ac5cf6c8aa326c2734e
Jason <jasonbconley@outlook.com>
2022-09-24 00:36:26 +0000

    Adding default dotnet API files with *almost* done config

---
+++ Data.cs
@@ -0,0 +1,8 @@
+namespace laserApp;
+
+public class Data
+{
+    public int Id { get; set; }
+
+    public string Text { get; set; }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,7 @@
+namespace laserApp.Models;
+
+public class Data
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+}
